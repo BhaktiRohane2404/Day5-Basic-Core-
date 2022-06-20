e01cb216d6875765589999adafe1048b70ccc4c1
BhaktiRohane2404 <98884739+BhaktiRohane2404@users.noreply.github.com>
2022-06-20 19:54:14 +0530

    Add files via upload

---
+++ factors.java
@@ -0,0 +1,29 @@
+package com.basicCoreProgram;
+
+import java.util.Scanner;
+
+public class factors {
+    public static void main(String args[]) {
+        System.out.println();
+        factors();
+    }
+    public static void factors() {
+        int num,temp;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the number");
+        num=s.nextInt();
+        temp=num;
+        if(num>0)
+        {
+            for(int i=2;i<Math.sqrt(num);i++)
+            {
+                while(temp%i==0) {
+                    System.out.println(i+"");
+                    temp=temp;
+                }
+            }
+        }
+        else
+            System.out.println("Enter valid number");
+    }
+}
